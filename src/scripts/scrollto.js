a7a9a7128efa9c5f0a3e05c16e0b93e76c3d20b2
Aleksey-7 <84351678+Aleksey-7@users.noreply.github.com>
2022-10-18 22:33:07 +0300

    add slow-scroll

---
+++ src/scripts/scrollto.js
@@ -0,0 +1,15 @@
+$('.scrollto a').on('click', function () {
+  let href = $(this).attr('href');
+
+  $('html, body').animate(
+    {
+      scrollTop: $(href).offset().top,
+    },
+    {
+      duration: 700, // по умолчанию «400»
+      easing: 'linear', // по умолчанию «swing»
+    }
+  );
+
+  return false;
+});
