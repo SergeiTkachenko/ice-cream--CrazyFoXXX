010921eac1f1a0b5cd8a9bfb1ceda957202ea188
SergeiTkachenko <92872700+SergeiTkachenko@users.noreply.github.com>
2022-10-20 09:05:56 +0000

    Deploying to gh-pages from @ SergeiTkachenko/ice-cream--CrazyFoXXX@d71a6dcd58a898c76ac299bf9c0bfded978a7564 🚀

--- index.f0957486.js
+++
@@ -1,2 +0,0 @@
-$(".gallery-slider").slick({autoplay:!0,autoplaySpeed:5e3,dots:!0,infinite:!0,speed:300,slidesToShow:1,adaptiveHeight:!0}),$(".one-time").slick({autoplay:!0,autoplaySpeed:5e3,dots:!0,infinite:!0,speed:300,slidesToShow:1,adaptiveHeight:!0});
-//# sourceMappingURL=index.f0957486.js.map
